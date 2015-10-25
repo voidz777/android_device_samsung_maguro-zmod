73d91cee4d89c9a75880de2b18deb11cf98bc958
voidz777 <voidz.evo3d@gmail.com>
2015-10-25 19:12:48 +0900

    eos bring-up

---
+++ eos.mk
@@ -0,0 +1,64 @@
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file is the build configuration for a full Android
+# build for maguro hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps). Except for a few implementation
+# details, it only fundamentally contains two inherit-product
+# lines, full and maguro, hence its name.
+#
+
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
+# Sounds for tuna
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage13.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+# This is where we'd set a backup provider if we had one
+#$(call inherit-product, device/sample/products/backup_overlay.mk)
+# Inherit from maguro device
+$(call inherit-product, device/samsung/maguro/device.mk)
+
+# Set those variables here to overwrite the inherited values.
+PRODUCT_NAME := eos_maguro
+PRODUCT_DEVICE := maguro
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Release name
+PRODUCT_RELEASE_NAME := Galaxy Nexus
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/eos/config/common.mk)
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.2.2/JDQ39/573038:user/release-keys"
+PRIVATE_BUILD_DESC="yakju-user 4.2.2 JDQ39 573038 release-keys"
+
+# Torch package
+#PRODUCT_PACKAGES += Torch
+
+# TouchControl package.
+#PRODUCT_PACKAGES += \
+#	TouchControl
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/eos/prebuilt/common/bootanimations/BOOTANIMATION-1280x768.zip:system/media/bootanimation.zip
